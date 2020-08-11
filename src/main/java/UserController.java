3db77464d6e8b0ca70eb7ec41c034cd7fda7bae6
maximrees <maximrees@gmail.com>
2020-08-11 14:06:21 +0200

    repo reset

--- src/main/java/UserController.java
+++ src/main/java/UserController.java
@@ -1,7 +1,9 @@
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class UserController {
     @Value("${spring.application.name}")
     String appName;
