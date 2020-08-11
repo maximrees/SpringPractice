87dc34d04e737be137a8bb5723a1c02c02305bb3
maxim rees <provocatie@msn.com>
2020-08-11 12:15:29 +0200

    build succesful, depencies good

---
+++ src/main/java/UserController.java
@@ -0,0 +1,14 @@
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class UserController {
+    @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String homePage(Model model){
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
